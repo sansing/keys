acfb7a16ba5a08f7c0ac62c7860ae8c4d5cd2abf
Stefan Ansing <stefan.ansing92@gmail.com>
2013-12-30 12:43:59 +0100

    Worked on some code documentation

--- src/keys/core.clj
+++ src/keys/core.clj
@@ -3,12 +3,23 @@
             [keys.key :refer :all]
             [slingshot.slingshot :refer :all]))
 
-(defn is-allowed? [subject k]
+(defn is-allowed?
+  "Given a subject checks if the subject is allowed to use a certain key."
+  [subject k]
   (let [subjectks (subject-keys subject)]
     (or (nil? k)
         (contains-key? k subjectks))))
 
-(defmacro secured [k & body]
+(defmacro secured
+  "Secures an expression with a given key. The current subject binding requires to have the permission to perform
+   the expression.
+
+   Either returns the value of the evaluated body, or throws one of the following slingshot exceptions:
+    - {:type :no-subject} ; The current subject has not been set
+    - {:type :invalid-key :key <k>} ; The key provided is not a valid key (it is not a vector)
+    - {:type :unauthorized :required <k> :subject <current-subject>} ; The current subject is not authorized to perform
+   the expression"
+  [k & body]
   `(let [k# ~k
          sbj# (subject)]
      (cond
